0783dcb6121107e515dfbf0e1a219cc613397824
wcsteve <wcsteve9@gmail.com>
2018-02-18 22:42:47 -0800

    Add simon to repository

---
+++ w2d1/skeleton/lib/simon.rb
@@ -0,0 +1,51 @@
+class Simon
+  COLORS = %w(red blue green yellow)
+
+  attr_accessor :sequence_length, :game_over, :seq
+
+  def initialize
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
+  end
+
+  def play
+    take_turn until @game_over
+      self.game_over_message
+      self.reset_game
+  end
+
+  def take_turn
+    self.show_sequence
+    self.require_sequence
+    self.round_success_message
+    @sequence_length += 1
+  end
+
+  def show_sequence
+    puts @sequence
+    add_random_color
+  end
+
+  def require_sequence
+    inputs = gets.chomp.downcase.split(' ')
+  end
+
+  def add_random_color
+    @seq << COLORS.sample
+  end
+
+  def round_success_message
+    puts "You passed!"
+  end
+
+  def game_over_message
+
+  end
+
+  def reset_game
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
+  end
+end
