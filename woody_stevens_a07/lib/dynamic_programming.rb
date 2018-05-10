977392baade237668ea315f87e226720d8b92df3
wcsteve <wcsteve9@gmail.com>
2018-05-10 11:56:18 -0700

    Add dp problems

--- woody_stevens_a07/lib/dynamic_programming.rb
+++ woody_stevens_a07/lib/dynamic_programming.rb
@@ -2,7 +2,8 @@ class DynamicProgramming
 
   def initialize
     @blair_cache = {1 => 1, 2 => 2}
-
+    @frog_cache = {1=>[[1]], 2=>[[1,1],[2]], 3=>[[1, 1, 1], [1, 2], [2, 1],[3]]}
+    @super_cache = {1=>[[1]], 2=>[[1,1],[2]], 3=>[[1, 1, 1], [1, 2], [2, 1],[3]]}
   end
 
   def blair_cache(n)
@@ -60,21 +61,33 @@ class DynamicProgramming
   end
 
   def frog_hops_top_down(n)
-    return [[1]] if n == 1
-    return [[1, 1], [2]] if n == 2
-    return [[1, 1, 1], [1, 2], [2, 1], [3]] if n == 3
-
-    return  frog_hops_top_down_helper(n - 1) +
-            frog_hops_top_down_helper(n - 2) +
-            frog_hops_top_down_helper(n - 3)
+    return @frog_cache[n] if @frog_cache[n]
+    cache_1 = frog_hops_top_down(n-1).map {|arr| arr + [1] }
+    cache_2 = frog_hops_top_down(n-2).map {|arr| arr + [2] }
+    cache_3 = frog_hops_top_down(n-3).map {|arr| arr + [3] }
+    @frog_cache[n] = cache_1 + cache_2 + cache_3
+    @frog_cache[n]
   end
 
   def frog_hops_top_down_helper(n)
-
+    return 1 if n === 1
+    return frog_hops_top_down_helper(n - 1)
   end
 
   def super_frog_hops(n, k)
-
+      return [[1]*n] if k == 1
+      k = n if k > n
+      frog = frog_hops_top_down_helper(n)
+      return nil if n > 999
+      return @super_cache[n] if @super_cache[n]
+      value = []
+      n.downto(2) do |idx|
+        if k >= (n-idx+1)
+          value += super_frog_hops(idx-1,k).map {|arr| [n-idx+1] + arr }
+        end
+      end
+      @super_cache[n] = value
+      @super_cache[n]
   end
 
   def knapsack(weights, values, capacity)
